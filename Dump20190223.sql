66dabdf9b8f2884301b9f213fbef13c4affbb574
RafaelGSS <rafael.nunu@hotmail.com>
2019-02-23 19:40:43 -0300

    sync

---
+++ Dump20190223.sql
@@ -0,0 +1,362 @@
+-- MySQL dump 10.13  Distrib 5.7.25, for Linux (x86_64)
+--
+-- Host: localhost    Database: geek4geek
+-- ------------------------------------------------------
+-- Server version	5.7.25-0ubuntu0.18.04.2
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `brands`
+--
+
+DROP TABLE IF EXISTS `brands`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `brands` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `brand_name` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `brands`
+--
+
+LOCK TABLES `brands` WRITE;
+/*!40000 ALTER TABLE `brands` DISABLE KEYS */;
+INSERT INTO `brands` VALUES (1,'NO_BRAND');
+/*!40000 ALTER TABLE `brands` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `categories`
+--
+
+DROP TABLE IF EXISTS `categories`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `categories` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `category_name` varchar(45) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` timestamp NULL DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `categories`
+--
+
+LOCK TABLES `categories` WRITE;
+/*!40000 ALTER TABLE `categories` DISABLE KEYS */;
+INSERT INTO `categories` VALUES (3,'series','2019-02-23 02:10:47',NULL),(4,'filmes','2019-02-23 02:10:47',NULL),(5,'games','2019-02-23 02:10:47',NULL);
+/*!40000 ALTER TABLE `categories` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `product_attributes`
+--
+
+DROP TABLE IF EXISTS `product_attributes`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `product_attributes` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id_product` int(11) NOT NULL,
+  `attribute_name` varchar(255) NOT NULL,
+  `attribute_value` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_product_attributes_1_idx` (`id_product`),
+  CONSTRAINT `fk_product_attributes_1` FOREIGN KEY (`id_product`) REFERENCES `products` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `product_attributes`
+--
+
+LOCK TABLES `product_attributes` WRITE;
+/*!40000 ALTER TABLE `product_attributes` DISABLE KEYS */;
+/*!40000 ALTER TABLE `product_attributes` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `product_categories`
+--
+
+DROP TABLE IF EXISTS `product_categories`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `product_categories` (
+  `id_product` int(11) NOT NULL,
+  `id_category` int(11) NOT NULL,
+  KEY `fk_product_categories_1_idx` (`id_product`),
+  KEY `fk_product_categories_2_idx` (`id_category`),
+  CONSTRAINT `fk_product_categories_1` FOREIGN KEY (`id_product`) REFERENCES `products` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `fk_product_categories_2` FOREIGN KEY (`id_category`) REFERENCES `categories` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `product_categories`
+--
+
+LOCK TABLES `product_categories` WRITE;
+/*!40000 ALTER TABLE `product_categories` DISABLE KEYS */;
+INSERT INTO `product_categories` VALUES (1,3);
+/*!40000 ALTER TABLE `product_categories` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `product_images`
+--
+
+DROP TABLE IF EXISTS `product_images`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `product_images` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id_product` int(11) NOT NULL,
+  `display_image` varchar(255) NOT NULL,
+  `low_display_image` varchar(255) DEFAULT NULL,
+  `big_display_image` varchar(255) DEFAULT NULL,
+  `alt` varchar(45) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_product_images_1_idx` (`id_product`),
+  CONSTRAINT `fk_product_images_1` FOREIGN KEY (`id_product`) REFERENCES `products` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `product_images`
+--
+
+LOCK TABLES `product_images` WRITE;
+/*!40000 ALTER TABLE `product_images` DISABLE KEYS */;
+INSERT INTO `product_images` VALUES (1,1,'https://assets.xtechcommerce.com/uploads/images/medium/3cc3f20bcff7146d500536bba1798780.jpg','https://assets.xtechcommerce.com/uploads/images/medium/3cc3f20bcff7146d500536bba1798780.jpg','https://assets.xtechcommerce.com/uploads/images/medium/3cc3f20bcff7146d500536bba1798780.jpg','Camiseta de Teste');
+/*!40000 ALTER TABLE `product_images` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `product_promotion`
+--
+
+DROP TABLE IF EXISTS `product_promotion`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `product_promotion` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id_product` int(11) NOT NULL,
+  `promotion_percentage` decimal(4,2) NOT NULL,
+  `is_active` tinyint(4) NOT NULL DEFAULT '1',
+  `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` timestamp NULL DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `id_product_UNIQUE` (`id_product`),
+  CONSTRAINT `fk_product_promotion_1` FOREIGN KEY (`id_product`) REFERENCES `products` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `product_promotion`
+--
+
+LOCK TABLES `product_promotion` WRITE;
+/*!40000 ALTER TABLE `product_promotion` DISABLE KEYS */;
+INSERT INTO `product_promotion` VALUES (1,1,10.00,1,'2019-02-23 05:17:48',NULL);
+/*!40000 ALTER TABLE `product_promotion` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `product_reviews`
+--
+
+DROP TABLE IF EXISTS `product_reviews`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `product_reviews` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id_product` int(11) NOT NULL,
+  `id_user` int(11) NOT NULL,
+  `review` varchar(255) NOT NULL,
+  `review_star` int(11) NOT NULL DEFAULT '5',
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  KEY `fk_product_reviews_1_idx` (`id_product`),
+  CONSTRAINT `fk_product_reviews_1` FOREIGN KEY (`id_product`) REFERENCES `products` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `product_reviews`
+--
+
+LOCK TABLES `product_reviews` WRITE;
+/*!40000 ALTER TABLE `product_reviews` DISABLE KEYS */;
+/*!40000 ALTER TABLE `product_reviews` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `product_tags`
+--
+
+DROP TABLE IF EXISTS `product_tags`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `product_tags` (
+  `id_product` int(11) NOT NULL,
+  `id_tag` int(11) NOT NULL,
+  KEY `fk_product_tags_1_idx` (`id_product`),
+  KEY `fk_product_tags_2_idx` (`id_tag`),
+  CONSTRAINT `fk_product_tags_1` FOREIGN KEY (`id_product`) REFERENCES `products` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `fk_product_tags_2` FOREIGN KEY (`id_tag`) REFERENCES `tags` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `product_tags`
+--
+
+LOCK TABLES `product_tags` WRITE;
+/*!40000 ALTER TABLE `product_tags` DISABLE KEYS */;
+/*!40000 ALTER TABLE `product_tags` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `product_variations`
+--
+
+DROP TABLE IF EXISTS `product_variations`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `product_variations` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id_product` int(11) NOT NULL,
+  `variation_name` varchar(255) NOT NULL,
+  `variation_value` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_product_variations_1_idx` (`id_product`),
+  CONSTRAINT `fk_product_variations_1` FOREIGN KEY (`id_product`) REFERENCES `products` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `product_variations`
+--
+
+LOCK TABLES `product_variations` WRITE;
+/*!40000 ALTER TABLE `product_variations` DISABLE KEYS */;
+/*!40000 ALTER TABLE `product_variations` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `products`
+--
+
+DROP TABLE IF EXISTS `products`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `products` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id_seller` int(11) DEFAULT NULL,
+  `id_brand` int(11) DEFAULT NULL,
+  `name` varchar(255) NOT NULL,
+  `unique_name` varchar(255) NOT NULL,
+  `display_description` varchar(45) NOT NULL,
+  `full_description` text NOT NULL,
+  `stock` int(11) NOT NULL DEFAULT '0',
+  `price` decimal(9,2) NOT NULL,
+  `seller_custom_field` varchar(255) DEFAULT NULL,
+  `is_active` tinyint(4) NOT NULL DEFAULT '1',
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` timestamp NULL DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  KEY `fk_products_1_idx` (`id_brand`),
+  KEY `fk_products_2_idx` (`id_seller`),
+  CONSTRAINT `fk_products_1` FOREIGN KEY (`id_brand`) REFERENCES `brands` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `fk_products_2` FOREIGN KEY (`id_seller`) REFERENCES `sellers` (`id`) ON DELETE SET NULL ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `products`
+--
+
+LOCK TABLES `products` WRITE;
+/*!40000 ALTER TABLE `products` DISABLE KEYS */;
+INSERT INTO `products` VALUES (1,1,1,'Produto de Teste 1','prod-test-1','Produto de teste','Produto de teste g4g',10,29.10,'AD7',1,'2019-02-23 01:22:47',NULL);
+/*!40000 ALTER TABLE `products` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `sellers`
+--
+
+DROP TABLE IF EXISTS `sellers`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `sellers` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `seller_name` varchar(255) NOT NULL,
+  `percentage_item` decimal(4,2) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `sellers`
+--
+
+LOCK TABLES `sellers` WRITE;
+/*!40000 ALTER TABLE `sellers` DISABLE KEYS */;
+INSERT INTO `sellers` VALUES (1,'GEEK4GEEK',99.00);
+/*!40000 ALTER TABLE `sellers` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `tags`
+--
+
+DROP TABLE IF EXISTS `tags`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `tags` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `tag_name` varchar(45) NOT NULL,
+  `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` timestamp NULL DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `tags`
+--
+
+LOCK TABLES `tags` WRITE;
+/*!40000 ALTER TABLE `tags` DISABLE KEYS */;
+/*!40000 ALTER TABLE `tags` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2019-02-23 16:41:21
--- controllers/ProductController.js
+++ controllers/ProductController.js
@@ -1,26 +1,43 @@
 var models = require('../models')
 
+/**
+ * Return minimal list of Products
+ */
 exports.list = (req, res, next) => {
     models.products.findAll({
         include: [
-            models.brands,
-            models.sellers, 
-            models.categories,
-            models.tags,
-            { model: models.product_images, as: 'images' },
-            { model: models.product_promotion, as: 'promotion' }
+            { model: models.brands },
+            { model: models.sellers },
+            { model: models.categories, attributes: ['category_name'], through: { attributes: [] } },
+            { model: models.tags },
+            { model: models.product_images, as: 'images', attributes: ['display_image', 'low_display_image', 'big_display_image', 'alt'] },
+            { model: models.product_promotion, as: 'promotion', attributes: ['promotion_percentage'] }
         ]
     }).then(product => {
         res.json(product)
-    })  
+    })
 }
 
+/**
+ * Return complete info about Product(Unique)
+ */
 exports.findByUnique = (req, res, next) => {
     const unique = req.params.unique
     models.products.findOne({
-        where: {unique_name: unique},
-        include: [models.brands, models.sellers]
+        include: [
+            { model: models.brands, attributes: ['brand_name'], raw: true },
+            { model: models.sellers, attributes: ['seller_name'] },
+            { model: models.categories, attributes: ['category_name'], through: { attributes: [] } },
+            { model: models.tags, attributes: ['tag_name'], through: { attributes: [] } },
+            { model: models.product_images, as: 'images', attributes: ['display_image', 'low_display_image', 'big_display_image', 'alt'] },
+            { model: models.product_promotion, as: 'promotion', attributes: ['promotion_percentage'] },
+        ],
+        attributes: {
+            exclude: ['id', 'created_at', 'updated_at', 'is_active']
+        },
+        where: { unique_name: unique, is_active: true }
     }).then(product => {
-        res.json(product)    
+        product.dataValues.promo = product.promotion != null
+        res.json(product)
     })
 }
