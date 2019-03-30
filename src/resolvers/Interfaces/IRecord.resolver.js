a8cf5b4456ec79f47b150afe0ac55aa13319dc43
RafaelGSS <rafael.nunu@hotmail.com>
2019-03-30 12:06:01 -0300

    feat: Added interfaces

---
+++ src/resolvers/Interfaces/IRecord.resolver.js
@@ -0,0 +1,7 @@
+module.exports = {
+    IRecord: {
+        __resolveType: (obj, context, info) => {
+            return false
+        }
+    }
+}
---
+++ src/resolvers/Interfaces/IResource.resolver.js
@@ -0,0 +1,7 @@
+module.exports = {
+    IResource: {
+        __resolveType: (obj, context, info) => {
+            return false
+        }
+    }
+}
