57de331775ec113a0237d30d6f97163333527276
RafaelGSS <rafael.nunu@hotmail.com>
2019-04-03 00:04:26 -0300

    feat: Added pagination by rows and items per page

--- src/resolvers/Product/ProductResource.resolver.js
+++ src/resolvers/Product/ProductResource.resolver.js
@@ -3,15 +3,18 @@ const { get } = require('lodash')
 
 module.exports = {
   ProductsResource: {
-    pagination: (parent, args, { cfg }) => {
+    pagination: (parent, args, { db, cfg }) => {
       const { page, per_page } = get(parent, 'baseArgs.pagination', cfg.PAGINATION_RESOURCE_DEFAULT)
 
-      return {
-        per_page: per_page,
-        current_page: page,
-        total_pages: 1,
-        total_records: 1
-      }
+      return db.table('products').count('id as total_records').first()
+        .then(({ total_records }) => {
+          return {
+            per_page: per_page,
+            current_page: page,
+            total_records: total_records,
+            total_pages: total_records / per_page
+          }
+        })
     },
     records: (parent, args, { db, knexPaginator, cfg }) => {
       const { page, per_page } = get(parent, 'baseArgs.pagination', cfg.PAGINATION_RESOURCE_DEFAULT)
