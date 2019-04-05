28a3c503a7e7e2eb0e84188b897c0c001dbe88ee
RafaelGSS <rafael.nunu@hotmail.com>
2019-04-05 01:06:23 -0300

    feat: Added base of relationship with images

--- src/resolvers/Product/Product.resolver.js
+++ src/resolvers/Product/Product.resolver.js
@@ -1,7 +1,20 @@
 module.exports = {
   Query: {
+    // Returns ProductResource
     products (parent, args) {
       return { baseArgs: args }
+    }
+  },
+  Product: {
+    images (parent, args, context, info) {
+      console.log(parent)
+      return [{
+        id: 21321,
+        display_image: '',
+        low_display_image: 'String',
+        big_display_image: 'String',
+        alt: 'String'
+      }]
     }
   }
 }
