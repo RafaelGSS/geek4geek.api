bdd88a143252768bd659b29e958f5f8ffb5baa69
RafaelGSS <rafael.nunu@hotmail.com>
2019-04-06 17:42:19 -0300

    feat: Add tags relationship

--- src/resolvers/Product/Product.resolver.js
+++ src/resolvers/Product/Product.resolver.js
@@ -11,6 +11,15 @@ module.exports = {
     },
     categories ({ id }, args, { db }) {
       return db('product_categories_view').where({ id_product: id })
+    },
+    promotions ({ id }, args, { db }) {
+      return db('product_promotion').where({ id_product: id })
+    },
+    reviews ({ id }, args, { db }) {
+      return db('product_reviews').where({ id_product: id })
+    },
+    tags ({ id }, args, { db }) {
+      return db('product_tags_view').where({ id_product: id })
     }
   }
 }
