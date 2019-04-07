d52a54937f2fe0aed2a1e682d188e7afa9867f3f
RafaelGSS <rafael.nunu@hotmail.com>
2019-04-06 21:40:11 -0300

    feat: Added attribute, variation, and brand relationship

--- src/resolvers/Product/Product.resolver.js
+++ src/resolvers/Product/Product.resolver.js
@@ -1,3 +1,4 @@
+/* eslint-disable camelcase */
 module.exports = {
   Query: {
     // Returns ProductResource
@@ -12,6 +13,9 @@ module.exports = {
     categories ({ id }, args, { db }) {
       return db('product_categories_view').where({ id_product: id })
     },
+    promo ({ id }, args, { db }) {
+      return db('product_promotion').where({ id_product: id, is_active: true }).orderBy('created_at', 'desc').first()
+    },
     promotions ({ id }, args, { db }) {
       return db('product_promotion').where({ id_product: id })
     },
@@ -20,6 +24,18 @@ module.exports = {
     },
     tags ({ id }, args, { db }) {
       return db('product_tags_view').where({ id_product: id })
+    },
+    variations ({ id }, args, { db }) {
+      return db('product_variations').where({ id_product: id })
+    },
+    attributes ({ id }, args, { db }) {
+      return db('product_attributes').where({ id_product: id })
+    },
+    brand ({ id_brand }, args, { db }) {
+      return db('brands').where({ id: id_brand }).first()
+    },
+    seller ({ id_seller }, args, { db }) {
+      return db('sellers').where({ id: id_seller }).first()
     }
   }
 }
